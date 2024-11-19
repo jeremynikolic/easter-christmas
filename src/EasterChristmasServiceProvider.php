d4deadf06e26f48a1e175de0c8a4dc39da15f13c
Jeremy Nikolic <jeremy@laravel.com>
2024-11-19 18:17:32 +0100

    Setup base dependencies

---
+++ src/EasterChristmasServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace VendorName\Skeleton;
+
+use Illuminate\Support\ServiceProvider;
+
+class EasterChristmasServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the package services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap the package services.
+     */
+    public function boot()
+    {
+        //
+    }
+}
